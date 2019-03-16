eb60864e8f59058e895bb107edb08147c930102a
GeOnE-357 <emedarias@gmail.com>
2019-03-15 21:05:01 -0300

    Algunos pequeños cambios

--- control/signals.py
+++ control/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from datetime import datetime, date, time, timedelta
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from incidente.models import Incidente, TipoIncidente, Parte, Cuerpo, TipoDistintivo
 from persona.models import Persona, InfoComp, Fisico, Foto
 from calzado.models import Calzado, FotoCalzado, Marca, Forma
 from .models import Log
@@ -125,6 +126,76 @@ def create_marca(sender, instance, raw, created, **kwargs):
 
 @receiver(post_save, sender=Forma)
 def create_forma(sender, instance, raw, created, **kwargs):
+	u=User.objects.order_by('-last_login').first() #Usuario que lo creo.
+	dni=str(u.username) 
+	nombre= str(u.first_name+" "+u.last_name)
+	tabla=str(instance._meta.db_table)#Nombre de la tabla.
+	objeto=int(instance.id)
+	fecha=datetime.today()
+	if created:
+		accion="CREAR"	
+	else:
+		accion="ACTUALIZAR"
+	Log.objects.create(dni=dni,nombre=nombre,accion=accion,tabla=tabla,objeto=objeto,fecha=fecha)
+
+@receiver(post_save, sender=Incidente)
+def create_incidente(sender, instance, raw, created, **kwargs):
+	u=User.objects.order_by('-last_login').first() #Usuario que lo creo.
+	dni=str(u.username) 
+	nombre= str(u.first_name+" "+u.last_name)
+	tabla=str(instance._meta.db_table)#Nombre de la tabla.
+	objeto=int(instance.id)
+	fecha=datetime.today()
+	if created:
+		accion="CREAR"	
+	else:
+		accion="ACTUALIZAR"
+	Log.objects.create(dni=dni,nombre=nombre,accion=accion,tabla=tabla,objeto=objeto,fecha=fecha)
+
+@receiver(post_save, sender=TipoIncidente)
+def create_tipoIncidente(sender, instance, raw, created, **kwargs):
+	u=User.objects.order_by('-last_login').first() #Usuario que lo creo.
+	dni=str(u.username) 
+	nombre= str(u.first_name+" "+u.last_name)
+	tabla=str(instance._meta.db_table)#Nombre de la tabla.
+	objeto=int(instance.id)
+	fecha=datetime.today()
+	if created:
+		accion="CREAR"	
+	else:
+		accion="ACTUALIZAR"
+	Log.objects.create(dni=dni,nombre=nombre,accion=accion,tabla=tabla,objeto=objeto,fecha=fecha)
+
+@receiver(post_save, sender=Parte)
+def create_parte(sender, instance, raw, created, **kwargs):
+	u=User.objects.order_by('-last_login').first() #Usuario que lo creo.
+	dni=str(u.username) 
+	nombre= str(u.first_name+" "+u.last_name)
+	tabla=str(instance._meta.db_table)#Nombre de la tabla.
+	objeto=int(instance.id)
+	fecha=datetime.today()
+	if created:
+		accion="CREAR"	
+	else:
+		accion="ACTUALIZAR"
+	Log.objects.create(dni=dni,nombre=nombre,accion=accion,tabla=tabla,objeto=objeto,fecha=fecha)
+
+@receiver(post_save, sender=TipoDistintivo)
+def create_tipoDistintivo(sender, instance, raw, created, **kwargs):
+	u=User.objects.order_by('-last_login').first() #Usuario que lo creo.
+	dni=str(u.username) 
+	nombre= str(u.first_name+" "+u.last_name)
+	tabla=str(instance._meta.db_table)#Nombre de la tabla.
+	objeto=int(instance.id)
+	fecha=datetime.today()
+	if created:
+		accion="CREAR"	
+	else:
+		accion="ACTUALIZAR"
+	Log.objects.create(dni=dni,nombre=nombre,accion=accion,tabla=tabla,objeto=objeto,fecha=fecha)
+
+@receiver(post_save, sender=Cuerpo)
+def create_cuerpo(sender, instance, raw, created, **kwargs):
 	u=User.objects.order_by('-last_login').first() #Usuario que lo creo.
 	dni=str(u.username) 
 	nombre= str(u.first_name+" "+u.last_name)
--- persona/filters.py
+++ persona/filters.py
@@ -1,10 +1,11 @@
 from .models import Persona
+from django.forms.widgets import TextInput
 import django_filters
 
 class PersonaFilter(django_filters.FilterSet):
-    nombre=django_filters.CharFilter(label=False,)
-    apellido=django_filters.CharFilter(label=False,)
-    dni=django_filters.NumberFilter(label=False,)
+    nombre=django_filters.CharFilter(label=False, widget=TextInput(attrs={'placeholder': 'Nombre...'}))
+    apellido=django_filters.CharFilter(label=False, widget=TextInput(attrs={'placeholder': 'Apellido...'}))
+    dni=django_filters.NumberFilter(label=False, widget=TextInput(attrs={'placeholder': 'Dni...'}))
     class Meta:
         model = Persona
         fields = ['nombre', 'apellido', 'dni']
